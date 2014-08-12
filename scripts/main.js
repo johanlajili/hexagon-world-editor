2e9c82f384cc9ac410d23fc33e9a56a356221247
jlaj <jlaj@innes.fr>
2014-08-12 17:59:04 +0200

    angular start

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,9 @@
-(function () {
-	var hexagonWorldEditor = angular.module('hexagonWorldEditor', [
-	  'ngRoute'
-	]);
-})();
+var hexagonWorldEditor = angular.module('hexagonWorldEditor', [
+  'ngRoute'
+]);
+hexagonWorldEditor.controller('MenuController', function($scope, $route, $routeParams, $location) {
+	$scope.$route = $route;
+	$scope.$location = $location;
+	$scope.$routeParams = $routeParams;
+});
+	
--- scripts/utils/importSystem.js
+++
@@ -1,9 +0,0 @@
-function addSelf(id) {
-	var me = document.currentScript.ownerDocument;
-	var view = $(me).find('#view-' + id);
-	var container = $(document).find('#' + id);
-	container.append(view);
-}
-(function () {
-	var hexagonView = document.registerElement('hexagon-view');
-})()
